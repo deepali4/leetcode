ca8c3ca285e6f669c989ff24a4e31ab2c7e7d90d
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-01-25 19:18:31 +0530

    Update 73-set-matrix-zeroes.java

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -31,3 +31,42 @@ class Solution {
 
 // BF - If any cell of the matrix has a zero we can record its row and column number. 
 // All the cells of this recorded row and column can be marked zero in the next iteration.
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        Set<Integer> setRow = new HashSet<>();
+        Set<Integer> setCol = new HashSet<>();
+        
+        for(int i =0 ;i< row;i++)
+        {
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    setRow.add(i);
+                    setCol.add(j);
+                }
+            }
+        }
+        
+         for(int i =0;i< row;i++)
+        {
+            for(int j=0;j<col;j++){
+                if(setRow.contains(i) || setCol.contains(j) ){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        
+    }
+}
+
+// BF - If any cell of the matrix has a zero we can record its row and column number. 
+// All the cells of this recorded row and column can be marked zero in the next iteration.
+// Complexity Analysis
+
+// Time Complexity: O(M \times N)O(M×N) where M and N are the number of rows and columns respectively.
+
+// Space Complexity: O(M + N)O(M+N).
