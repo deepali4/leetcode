dbab3d837530e21f4c61b068e60988f036631773
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-02-09 18:24:37 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    
+    
+    
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        
+        int[][] arr = new int [n][2];
+        
+        for(int i =0 ;i<n;i++)
+            {
+                arr[i][0]=start[i];
+                arr[i][1]=end[i];
+
+            }
+            
+            Arrays.sort(arr,(a,b)->a[1]-b[1]);
+        
+        int count=1;
+        int limit = arr[0][1]; // end time of 1 st meet
+        for(int i = 1 ;i<n;i++)
+        {
+            
+            
+            if(arr[i][0]>limit)  // from 2nd meet compare start with prev end time
+            {
+                count++;
+                limit=arr[i][1];
+            }
+            
+        }
+        
+        return count;
+        
+        // add your code here
+    }
+}
+
