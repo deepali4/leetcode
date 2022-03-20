801a0b1d55fa785d4dc28812c30416cb5b39a264
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-03-20 22:01:21 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        ArrayList<Integer>  ans  = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        int[] vi= new int [V];
+        
+        vi[0]=1;
+        q.add(0);
+        
+        while(!q.isEmpty())
+        {
+            
+            int num =q.poll();
+            ans.add(num);
+            
+            for(int it :  adj.get(num)){
+                if(vi[it]==0){
+                    q.add(it);
+                    vi[it]=1;
+                }
+                
+            }
+            
+            
+            
+        }
+       
+       
+       
+       return ans;
+    }
+}
