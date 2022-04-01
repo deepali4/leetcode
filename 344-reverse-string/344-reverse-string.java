738145ce28d3d33714947cacfae9569c918ea969
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-04-01 12:01:55 +0530

    Time: 2 ms (33.86%), Space: 58 MB (15.94%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int ptr1 = 0;
+        int ptr2=s.length-1;
+        
+        while(ptr1<ptr2)
+        {
+            char temp = s[ptr1];
+            s[ptr1]=s[ptr2];
+            s[ptr2]=temp;
+            ptr1++;
+            ptr2--;
+        }
+    }
+}
