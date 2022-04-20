67f0e111d18ed2e1f69b68b46bbc29301cbdcf9e
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-04-21 01:42:10 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (80.68%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,20 +1,14 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
-        if(m==1 && n==1) return 1;
-        
-        int[][]dp = new int [m][n];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        
-         countPath(0,0,m,n,dp);
-        return dp[0][0];
-      
+public class Solution {
+public int uniquePaths(int m, int n) {
+    int[][] grid = new int[m][n];
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            if(i==0||j==0)
+                grid[i][j] = 1;
+            else
+                grid[i][j] = grid[i][j-1] + grid[i-1][j];
+        }
     }
-    public int countPath(int i , int j, int m ,int n ,int[][]dp)
-    {
-        if(i== (m-1) && j == (n-1)) return 1;
-        if(i>=m || j >= n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        else return dp[i][j] =countPath(i+1,j,m,n,dp)+countPath(i,j+1,m,n,dp);
-    }
+    return grid[m-1][n-1];
+}
 }
