912c4370d7ebd302c9acf40f1bf360bfda69085b
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-09-02 19:23:56 +0530

    Time: 2 ms (18.97%), Space: 41.9 MB (19.22%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,21 +1,21 @@
 class Solution {
+    HashMap<Integer ,Integer > memo = new HashMap<>();
     public int rob(int[] nums) {
         
-        if(nums.length==0)
-             return 0;
-         if(nums.length==1)
-             return nums[0];
-        int[] dp = new int[nums.length];
+        return dp(nums.length-1,nums);
         
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        
-        
-        for(int i = 2; i < nums.length ; i++){
-            
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
-            
-        }      
-        return dp[nums.length-1];
     }
+    
+        
+    private int dp(int i,int[] nums) {
+        // Base cases
+        if (i == 0) return nums[0];
+        if (i == 1) return Math.max(nums[0], nums[1]);
+        if (!memo.containsKey(i)) {
+            memo.put(i, Math.max(dp(i - 1,nums), dp(i - 2,nums) + nums[i])); // Recurrence relation
+        }
+        return memo.get(i);
+    }
+        
+    
 }
