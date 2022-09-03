279d37f5b12821d3a7d310b798d78e239f100156
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-09-03 19:16:53 +0530

    Time: 690 ms (26.36%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,17 @@
 # Write your MySQL query statement below
 
-SELECT name as Employee from Employee e  where e.managerId is not null and salary > (
+# SELECT name as Employee from Employee e  where e.managerId is not null and salary > (
 
-    Select  salary from Employee  where id = e.managerId
-)
+#     Select  salary from Employee  where id = e.managerId
+# )
+
+
+SELECT
+    a.Name AS 'Employee'
+FROM
+    Employee AS a,
+    Employee AS b
+WHERE
+    a.ManagerId = b.Id
+        AND a.Salary > b.Salary
+;
