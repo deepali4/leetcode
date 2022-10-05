3f3708c9c8a8c572dfc02628c494e590ce7bc04c
deepali4 <36330859+deepali4@users.noreply.github.com>
2022-10-05 17:05:58 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (76.22%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,16 +14,14 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-     return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);   
-    }
-     boolean solve(TreeNode root,long left ,long right)
-    {
-        if(root==null)
-            return true;
-        if(root.val<=left||root.val>=right)
-            return false;
-        return solve(root.left,left,root.val)&& solve(root.right,root.val,right);
+    public boolean isValidBST(TreeNode root) {        
+      return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
     
+    public boolean  solve(TreeNode root ,long left ,long right){
+        
+        if(root==null) return true;
+        if(root.val<=left|| root.val>=right) return false;
+        return solve(root.left,left,root.val) && solve(root.right,root.val,right);
+    }
 }
